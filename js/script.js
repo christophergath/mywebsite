7f4f1b83abb95d11a801a7c11629d092c97ddea9
christophergath <gath@mail.de>
2018-11-12 19:40:10 +0100

    v4.0 with comments and cleaned up

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
+// ###############################################################
+// ##################  IMPORTS  ###############################
+// ###############################################################
+
+// ###############################################################
+// ##################  VARIABLES   ###############################
+// ###############################################################
+
 var navItems = document.getElementsByClassName('nav-item');
 var navStyle = document.getElementsByTagName('nav');
 var aTag = document.getElementsByTagName('a');
 
+// ###############################################################
+// ################## CODE #######################################
+// ###############################################################
 
+// Receive devices Viewportheight with the maximum of .clientHeight or .innerHeight
 
 var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 console.log(viewportHeight);
 
+// Devide 9.1 of it to get the nice effect later
+
 var partialViewportHeight = viewportHeight / 9.1;
-
-
-function setNavbar(){
-
-}
 
 
 
@@ -23,33 +32,31 @@ function doSomething(scroll_pos) {
 
     //
 
-  if (scroll_pos < viewportHeight-partialViewportHeight){
-      navItems[0].classList.add('activeli','animated','fadeInDown');
-      navItems[1].classList.remove('activeli','animated','fadeInDown');
-      navItems[3].classList.remove('activeli','animated','fadeInDown');
-      navItems[4].classList.remove('activeli','animated','fadeInDown');
+    if (scroll_pos < viewportHeight - partialViewportHeight) {
+        navItems[0].classList.add('activeli', 'animated', 'fadeInDown');
+        navItems[1].classList.remove('activeli', 'animated', 'fadeInDown');
+        navItems[3].classList.remove('activeli', 'animated', 'fadeInDown');
+        navItems[4].classList.remove('activeli', 'animated', 'fadeInDown');
 
 
-      navStyle[0].classList.add('bg1');
-      navStyle[0].classList.remove('bg2');
-      navStyle[0].classList.remove('bg3');
-      navStyle[0].classList.remove('bg4');
+        navStyle[0].classList.add('bg1');
+        navStyle[0].classList.remove('bg2');
+        navStyle[0].classList.remove('bg3');
+        navStyle[0].classList.remove('bg4');
 
-      aTag[0].style.color = "black";
-      aTag[1].style.color = "black";
-      aTag[2].style.color = "black";
-      aTag[3].style.color = "black";
-      aTag[4].style.color = "black";
+        aTag[0].style.color = "black";
+        aTag[1].style.color = "black";
+        aTag[2].style.color = "black";
+        aTag[3].style.color = "black";
+        aTag[4].style.color = "black";
 
 
-  }
-
-    else if(scroll_pos > viewportHeight-partialViewportHeight && scroll_pos < viewportHeight*2-partialViewportHeight){
-        console.log("Scrollolol position ="+scroll_pos);
-        navItems[0].classList.remove('activeli','animated','fadeInDown');
-        navItems[1].classList.add('activeli','animated','fadeInDown');
-        navItems[3].classList.remove('activeli','animated','fadeInDown');
-        navItems[4].classList.remove('activeli','animated','fadeInDown');
+    } else if (scroll_pos > viewportHeight - partialViewportHeight && scroll_pos < viewportHeight * 2 - partialViewportHeight) {
+        console.log("Scrollolol position =" + scroll_pos);
+        navItems[0].classList.remove('activeli', 'animated', 'fadeInDown');
+        navItems[1].classList.add('activeli', 'animated', 'fadeInDown');
+        navItems[3].classList.remove('activeli', 'animated', 'fadeInDown');
+        navItems[4].classList.remove('activeli', 'animated', 'fadeInDown');
 
         navStyle[0].classList.remove('bg1');
         navStyle[0].classList.add('bg2');
@@ -58,22 +65,20 @@ function doSomething(scroll_pos) {
 
 
         aTag[0].style.color = "black";
-      aTag[1].style.color = "black";
-      aTag[2].style.color = "black";
-      aTag[3].style.color = "black";
-      aTag[4].style.color = "black";
+        aTag[1].style.color = "black";
+        aTag[2].style.color = "black";
+        aTag[3].style.color = "black";
+        aTag[4].style.color = "black";
 
 
 
 
-    }
-
-    else if(scroll_pos > viewportHeight*2-partialViewportHeight && scroll_pos < viewportHeight*3-partialViewportHeight){
-        console.log("Scrollolol position ="+scroll_pos);
-        navItems[0].classList.remove('activeli','animated','fadeInDown');
-        navItems[1].classList.remove('activeli','animated','fadeInDown');
-        navItems[3].classList.add('activeli','animated','fadeInDown');
-        navItems[4].classList.remove('activeli','animated','fadeInDown');
+    } else if (scroll_pos > viewportHeight * 2 - partialViewportHeight && scroll_pos < viewportHeight * 3 - partialViewportHeight) {
+        console.log("Scrollolol position =" + scroll_pos);
+        navItems[0].classList.remove('activeli', 'animated', 'fadeInDown');
+        navItems[1].classList.remove('activeli', 'animated', 'fadeInDown');
+        navItems[3].classList.add('activeli', 'animated', 'fadeInDown');
+        navItems[4].classList.remove('activeli', 'animated', 'fadeInDown');
 
 
         navStyle[0].classList.remove('bg2');
@@ -89,14 +94,12 @@ function doSomething(scroll_pos) {
 
 
 
-    }
-
-     else if(scroll_pos > viewportHeight*3-partialViewportHeight ){
-        console.log("Scrollolol position ="+scroll_pos);
-        navItems[0].classList.remove('activeli','animated','fadeInDown');
-        navItems[1].classList.remove('activeli','animated','fadeInDown');
-        navItems[3].classList.remove('activeli','animated','fadeInDown');
-        navItems[4].classList.add('activeli','animated','fadeInDown');
+    } else if (scroll_pos > viewportHeight * 3 - partialViewportHeight) {
+        console.log("Scrollolol position =" + scroll_pos);
+        navItems[0].classList.remove('activeli', 'animated', 'fadeInDown');
+        navItems[1].classList.remove('activeli', 'animated', 'fadeInDown');
+        navItems[3].classList.remove('activeli', 'animated', 'fadeInDown');
+        navItems[4].classList.add('activeli', 'animated', 'fadeInDown');
 
 
         navStyle[0].classList.remove('bg1');
@@ -104,7 +107,7 @@ function doSomething(scroll_pos) {
         navStyle[0].classList.remove('bg2');
         navStyle[0].classList.remove('bg3');
 
-         aTag[0].style.color = "white";
+        aTag[0].style.color = "white";
         aTag[1].style.color = "white";
         aTag[2].style.color = "white";
         aTag[3].style.color = "white";
@@ -112,19 +115,19 @@ function doSomething(scroll_pos) {
     }
 }
 
-window.addEventListener('scroll', function(e) {
+window.addEventListener('scroll', function (e) {
 
-  last_known_scroll_position = window.scrollY;
+    last_known_scroll_position = window.scrollY;
 
-  if (!ticking) {
+    if (!ticking) {
 
-    window.requestAnimationFrame(function() {
-      doSomething(last_known_scroll_position);
-      ticking = false;
-    });
+        window.requestAnimationFrame(function () {
+            doSomething(last_known_scroll_position);
+            ticking = false;
+        });
 
-    ticking = true;
+        ticking = true;
 
-  }
+    }
 
 });
